a2bdd149f402d45a1b38fc4e53a260d9ce57d5d1
hgparkk <phg2559@naver.com>
2023-09-18 11:56:09 +0900

    new commit

---
+++ cp1/Sample1_1.java
@@ -0,0 +1,8 @@
+class Sample1_1
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Java 세상에 오신것을 환영합니다");
+        System.out.println("Java를 시작합니다");
+    }
+}
---
+++ cp1/Sample1_2.java
@@ -0,0 +1,8 @@
+class Sample1_2 
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Java 세상에 오신것을 환영합니다");
+        System.out.print("Java를 시작합니다");
+    }
+}
